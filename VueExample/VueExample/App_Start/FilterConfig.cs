9e2e98d1067f542bfed9a45189170084e0c33be0
Mesut KAYA <mesutkaya@anadolu.edu.tr>
2021-02-24 08:41:28 +0300

    First commit.

---
+++ VueExample/VueExample/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace VueExample
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
